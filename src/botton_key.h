77a80544bf98e535dee38d9b228065510632df29
Rramon <rodrigo.n.ramon@gmail.com>
2023-03-23 09:46:25 -0400

    Delete botton_key.h

--- src/botton_key.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <termios.h>
-#include <sys/time.h>
-#include <sys/types.h>
-
-int Button_Init(void);
-int Button_detection(void);
