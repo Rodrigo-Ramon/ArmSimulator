789dd3bfaba2d0153e9a2010baa2e8947b9e5079
Rramon <rodrigo.n.ramon@gmail.com>
2023-04-03 12:54:00 -0400

    Delete src directory

--- src/multitask_make.sh
+++
@@ -1,7 +0,0 @@
-rm main
-g++ -c CAN_BUS_HCPS.c
-g++ -c keyboard_event.c
-g++ -c IMU_WT901.c
-g++ -c main.cpp 
-g++ -lpthread -lrt -lm CAN_BUS_HCPS.o keyboard_event.o main.o IMU_WT901.o -o main
-rm CAN_BUS_HCPS.o keyboard_event.o IMU_WT901.o main.o
